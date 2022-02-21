ac7272de8a79671e8b3dae7fb1b6f9b8f17600ca
Ubuntu <azureuser@qaproject.f3mhg2odat5unahc0xjwu3vpwb.zx.internal.cloudapp.net>
2022-02-21 16:44:47 +0000

    database

---
+++ application/__init__.py
@@ -0,0 +1,11 @@
+#import flask from flask module
+#import SQLALCHEMY CLASS FROM SQLALCHEMY MODULE
+# import OS
+#importt getenv from OS
+
+#create instance of flask and store it in app
+app =
+
+#create and store enviornment variables
+
+#import routes
